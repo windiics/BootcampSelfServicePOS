6d05f770c9aac0ca44966fc9f17a30aebdbdf61c
windiics <21030042@stmik-mi.ac.id>
2023-11-29 18:09:43 +0700

    Submission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,16 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            "name"  => "administrator",
+            "email" => "admin@admin.com",
+            "password" => Hash::make("admin123"),
+            "role_id" => 1
+        ]);
+
+        $this->call([
+            RoleSeeder::class
+        ]);
     }
 }
